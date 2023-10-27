8fab04489e28ccd41c28bbde8c1d69726907f09e
Eben <maradebenezer@gmail.com>
2023-10-27 02:34:54 +0100

    fixed 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -5,6 +5,7 @@
     Least Recently Used (LRU) algo
 """
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LRUCache(BaseCaching):
@@ -20,42 +21,21 @@ class LRUCache(BaseCaching):
 
     def __init__(self):
         """ initialize with the parent """
-        self.data = []
-        # data is a list of lists [key, value, age]
-        self.age_bit = 0  # age bit of items
-
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """ insert into cache using LRU """
-        all_keys = [item[0] for item in self.data]
-        # confirm not none
-        if key is not None and item is not None:
-            # if a hit
-            if key in all_keys:
-                for i in range(len(self.data)):
-                    # if key match is found, update
-                    if self.data[i][0] == key:
-                        self.data[i][1] = item
-                        self.data[i][2] = self.age_bit
-                        self.age_bit += 1
-                        break
-            else:  # it's a miss
-                if len(self.data) >= LRUCache.MAX_ITEMS:  # cache is full
-                    # delete youngest item
-                    ages = [item[2] for item in self.data]
-                    to_del = self.data[ages.index(min(ages))]
-                    print("DISCARD: {}".format(to_del[0]))
-                    del self.data[ages.index(min(ages))]
-                # add the new data
-                self.data.append([key, item, self.age_bit])
-                self.age_bit += 1
-
-        # reload self.cache_data
-        self.cache_data = {}
-        for k, v, _ in self.data:
-            self.cache_data[k] = v
-        print(self.data)
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """ retrieve item """
