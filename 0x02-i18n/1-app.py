210694eefc54d05b65925145f2a31ba445b663cf
Eben <maradebenezer@gmail.com>
2023-11-03 16:24:27 +0100

    added 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 """ Simple app with route showing welcome message """
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ configuration settings """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @app.route("/")
 def index() -> str:
     """ index route """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
